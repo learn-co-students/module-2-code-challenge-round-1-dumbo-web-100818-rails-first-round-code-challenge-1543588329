7864bab877ae209e0d0398f41648ea175e6db04d
cfernando415 <ca.fernando415@gmail.com>
2018-11-30 10:54:16 -0500

    done with all requirements

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,20 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+  def create
+    @heroine = Heroine.create(params.require(:heroine).permit(:name, :super_name, :power_id))
+    if @heroine.valid?
+      redirect_to heroines_path
+    else
+      @powers = Power.all
+      render 'new'
+    end
+  end
 end
