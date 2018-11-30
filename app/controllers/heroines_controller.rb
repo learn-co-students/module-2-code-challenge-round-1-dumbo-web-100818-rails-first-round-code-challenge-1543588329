43405be9d3c1f8e1528b40e222ba6237c0bc0139
La Fountain <github email address>
2018-11-30 11:24:22 -0500

    Complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,33 @@
+require 'byebug'
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    # byebug
+    if @heroine.save
+      render :show
+    else
+      flash[:error] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
